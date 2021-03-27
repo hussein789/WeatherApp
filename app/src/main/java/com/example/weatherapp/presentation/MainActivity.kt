62393c516ff5dcfac361b9b375eff7ccd60e9d24
hussain <hussein.antar@vezeeta.com>
2021-03-27 17:45:45 +0200

    move MainActivity.kt into presentation package

--- app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.weatherapp
+package com.example.weatherapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        Utils.setLightStatusBar(binding.root,this)
         setContentView(binding.root)
     }
 }
